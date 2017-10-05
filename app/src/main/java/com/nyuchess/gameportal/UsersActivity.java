778aa7913b55609440ebfaa4153bf794082c64b8
Jordan Green <thegreenjordan@gmail.com>
2017-10-05 01:33:48 -0400

    clean up code

--- app/src/main/java/com/nyuchess/gameportal/UsersActivity.java
+++ app/src/main/java/com/nyuchess/gameportal/UsersActivity.java
@@ -3,13 +3,9 @@ package com.nyuchess.gameportal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.firebase.client.Firebase;
-import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,13 +25,13 @@ public class UsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_users);
 
         List<String> usernames = new ArrayList<>();
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.user, usernames);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.user, usernames);
 
         final ListView onlineUsersList = findViewById(R.id.online_users_list);
         onlineUsersList.setAdapter(adapter);
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
-        // Attach a listener to read the data at our posts reference
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -47,13 +43,11 @@ public class UsersActivity extends AppCompatActivity {
                     if (isConnected.getValue() == null || isConnected.getValue().equals("False")){
                         continue;
                     }
+                    String username = user.child("public_fields").child("displayName")
+                            .getValue().toString();
 
-                    Log.d(TAG, user.getKey());
-                    DataSnapshot publicFields = user.child("public_fields");
-                    Log.d(TAG, publicFields.getKey());
-                    DataSnapshot displayName = publicFields.child("displayName");
-                    Log.d(TAG, displayName.getKey());
-                    adapter.add(displayName.getValue().toString());
+                    Log.d(TAG, username);
+                    adapter.add(username);
                 }
             }
 
@@ -62,47 +56,5 @@ public class UsersActivity extends AppCompatActivity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
-
-
-//        FirebaseListAdapter<User> adapter = new FirebaseListAdapter<User>(this, User.class,
-//                R.layout.user, FirebaseDatabase.getInstance().getReference("/users")) {
-//            @Override
-//            protected void populateView(View v, User user, int position) {
-//                Log.d(TAG, user.toString());
-//                Log.d(TAG, user.getDisplayName());
-//                // Get references to the views of user.xml
-//                TextView usernameText = v.findViewById(R.id.username);
-//
-//                // Set their text
-//                usernameText.setText(user.getDisplayName());
-//            }
-//        };
-
-        onlineUsersList.setAdapter(adapter);
-
-//        Firebase ref = new Firebase("");
-//        ref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot snapshot) {
-//                ArrayList<User> onlineUsers = new ArrayList<>():
-//                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-//                    //Getting the data from snapshot
-//                    User user = postSnapshot.getValue(User.class);
-//
-//                    //add person to your list
-//                    onlineUsers.add(user);
-//                    //create a list view, and add the apapter, passing in your list
-//
-//                }
-//
-//                onlineUsersList.setAdapter(onlineUsers);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                System.out.println("The read failed: " + databaseError.getMessage());
-//            }
-//        });
-
     }
 }
