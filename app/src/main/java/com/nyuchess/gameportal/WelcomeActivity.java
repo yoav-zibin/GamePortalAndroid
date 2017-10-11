8f974af1a88f2d25273486fe931154f499fe6975
Victor Treaba <vtreaba@gmail.com>
2017-10-10 22:39:17 -0400

    Fixes
    Properly adds to DB
    Users online works except for on first boot
    See people online/offline no longer goes off page

--- app/src/main/java/com/nyuchess/gameportal/WelcomeActivity.java
+++ app/src/main/java/com/nyuchess/gameportal/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.nyuchess.gameportal;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -35,6 +37,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     private String UID;
     private int mOnlineViewerCount;
     private TextView mOnlineViewerCountTextView;
+    private int people = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,19 +57,29 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
         mAuth = FirebaseAuth.getInstance();
 
-        String[] data = username.split(" ");
+        UID = mAuth.getCurrentUser().getUid();
 
-        UID = data[1];
+        DatabaseReference ref = database.getReference("/users");
 
-        DatabaseReference pubRef = database.getReference("/users/" + UID + "/publicFields");
-        DatabaseReference privRef = database.getReference("/users/" + UID + "/privateFields");
+        Map<String, Object> pub = new HashMap<>();
+        pub.put("avatarImageUrl", "https://i0.wp.com/vanillicon.com/f6562c80843af5711653fc76170ddfd6_200.png?ssl=1");
+        String disp = mAuth.getCurrentUser().getDisplayName();
+        if(disp == null || disp.length() < 1) {
+            disp = "TBD";
+        }
+        pub.put("displayName", disp);
+        pub.put("isConnected", true);
+        pub.put("lastSeen", ServerValue.TIMESTAMP);
 
-        pubRef.child("avatarImageUrl").setValue("");
-        pubRef.child("displayName").setValue(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
-        pubRef.child("isConnected").setValue("True");
-        pubRef.child("lastSeen").setValue("");
+        Map<String, Object> priv = new HashMap<>();
+        priv.put("email", mAuth.getCurrentUser().getEmail());
+        priv.put("createdOn", ServerValue.TIMESTAMP);
 
-        privRef.child("email").setValue(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("publicFields", pub);
+        fields.put("privateFields", priv);
+
+        ref.child(mAuth.getCurrentUser().getUid()).setValue(fields);
 
         initialiseOnlinePresence();
     }
@@ -104,7 +117,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
         DatabaseReference pubRef = database.getReference("/users/" + data[1] + "/publicFields");
 
-        pubRef.child("isConnected").setValue("False");
+        pubRef.child("isConnected").setValue("false");
         pubRef.child("lastSeen").setValue(ServerValue.TIMESTAMP);
 
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
@@ -119,19 +132,17 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                         }
                     });
         }
-
-        database.getReference().child("/presence/" + UID).removeValue();
     }
 
     private void initialiseOnlinePresence() {
+        people = 0;
         final DatabaseReference databaseReference = database.getReference();
 
         final DatabaseReference onlineRef = databaseReference.child(".info/connected");
-        final DatabaseReference currentUserRef = databaseReference.child("/presence/" + UID);
+        final DatabaseReference currentUserRef = databaseReference.child("/recentlyConnected/" + UID);
         final DatabaseReference statusRef = databaseReference.child("/users/" + UID + "/publicFields/isConnected");
         final DatabaseReference lastSeenRef = databaseReference.child("/users/" + UID + "/publicFields/lastSeen");
 
-        // Add self to list of recently connected users
         final DatabaseReference recentRef = databaseReference.child("/recentlyConnected");
         Map<String, Object> me = new HashMap<>();
         me.put("uid", UID);
@@ -144,9 +155,9 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 Log.d(TAG, "DataSnapshot:" + dataSnapshot);
                 if (dataSnapshot.getValue(Boolean.class)) {
                     lastSeenRef.onDisconnect().setValue(ServerValue.TIMESTAMP);
-                    statusRef.onDisconnect().setValue("False");
-                    currentUserRef.onDisconnect().removeValue();
-                    currentUserRef.setValue(true);
+                    //statusRef.onDisconnect().setValue("false");
+                    //currentUserRef.onDisconnect().removeValue();
+                    //currentUserRef.setValue(true);
                 }
             }
 
@@ -159,34 +170,30 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         onlineViewersCountRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(final DataSnapshot dataSnapshot) {
-                Log.d(TAG, "DataSnapshot:" + dataSnapshot);
+                people = 0;
+                Log.d(TAG, " NUMBER USERS " + dataSnapshot.getChildrenCount() + " " + people);
                 for (DataSnapshot user: dataSnapshot.getChildren()){
                     final String userid = (String) user.child("uid").getValue();
-                    DatabaseReference isOnlineRef = databaseReference.child("/users/" + userid + "/publicFields/isConnected");
+                    DatabaseReference isOnlineRef = databaseReference.child("/users/" + userid + "/publicFields");
                     isOnlineRef.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.getValue() == null){
-                                return;
-                            }
-                            Log.d(TAG, userid + " " + dataSnapshot.getValue().toString());
-                            if (dataSnapshot.getValue().toString().equals("True")){
-                            // do this in separate methods since inner classes need it to be final for direct references
-                                incrementUserCount();
-                            }
-                            else {
-                                decrementUserCount();
+                            if(dataSnapshot.child("isConnected").getValue() != null) {
+                                Log.d(TAG, dataSnapshot.child("isConnected").getValue().toString());
+                                if(dataSnapshot.child("isConnected").getValue().toString().equals("true")) {
+                                    addPeople();
+                                    Log.d(TAG, "adding " + people + " " + userid);
+                                    mOnlineViewerCountTextView.setText("Users Online: " + people);
+                                }
                             }
                         }
 
                         @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            Log.d(TAG, "DatabaseError:" + databaseError);
+                        public void onCancelled(DatabaseError error) {
+
                         }
                     });
-
                 }
-                mOnlineViewerCountTextView.setText("Users Online: " + mOnlineViewerCount);
             }
 
             @Override
@@ -196,13 +203,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         });
     }
 
-    private void incrementUserCount(){
-        mOnlineViewerCount++;
-        Log.d(TAG, "User online, " + mOnlineViewerCount);
-    }
-
-    private void decrementUserCount(){
-        mOnlineViewerCount--;
-        Log.d(TAG, "User offline, " + mOnlineViewerCount);
+    private void addPeople() {
+        people++;
     }
 }
